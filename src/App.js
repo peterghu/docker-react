2fe72d4844197da69b4619975e0e25db6b997c18
peterghu <peterh782@gmail.com>
2020-10-24 17:44:55 -0600

    add Learn React link back in

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
         <p>
           Feature test. Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
